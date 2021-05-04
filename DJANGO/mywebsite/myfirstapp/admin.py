2ebc25cdf2e765d7d265cd1669e7f4dda9b32f8b
Aile <tabataaile@gmail.com>
2021-05-03 19:07:11 -0500

    borrar D

--- DJANGO/mywebsite/myfirstapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Estudiante, Carrera
-
-# Register your models here.
-
-admin.site.register(Estudiante)
-admin.site.register(Carrera)
--- DJANGO/mywebsite/myfirstapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index,name="index"),
-    path('<int:estudiante_id>/detalles/', views.detalles,name="detalles"),
-    path('<int:estudiante_id>/carreras/', views.carreras,name="carreras"),
-    path('<int:estudiante_id>/agrega_carrera/', views.agrega_carrera,name="agrega_carrera"),
-]
